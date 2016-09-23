77c540fd6ec6c3dd5cef5b8a4824c1d0b2582a4c
Tpt <thomaspt@hotmail.fr>
2016-09-23 17:48:48 +0200

    Makes sure to don't process dumps twice even after a restart

--- src/main/java/us/askplatyp/kb/lucene/wikidata/WikidataLuceneIndexFactory.java
+++ src/main/java/us/askplatyp/kb/lucene/wikidata/WikidataLuceneIndexFactory.java
@@ -5,18 +5,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.wikidata.wdtk.dumpfiles.DumpContentType;
 import org.wikidata.wdtk.dumpfiles.DumpProcessingController;
+import org.wikidata.wdtk.dumpfiles.MwDumpFile;
+import org.wikidata.wdtk.dumpfiles.wmf.WmfDumpFileManager;
 import us.askplatyp.kb.lucene.Configuration;
 import us.askplatyp.kb.lucene.http.Main;
 import us.askplatyp.kb.lucene.lucene.LuceneIndex;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Collections;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 /**
  * @author Thomas Pellissier Tanon
@@ -24,6 +30,7 @@ import java.util.concurrent.TimeUnit;
 public class WikidataLuceneIndexFactory implements Factory<LuceneIndex> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WikidataLuceneIndexFactory.class);
+    private static final LastProcessedDumpInfo LAST_PROCESSED_DUMP_INFO = new LastProcessedDumpInfo();
 
     private static LuceneIndex index;
 
@@ -44,20 +51,38 @@ public class WikidataLuceneIndexFactory implements Factory<LuceneIndex> {
                 null,
                 true
         );
-        dumpProcessingController.processMostRecentJsonDump();
-        index.refreshReaders();
-        dumpProcessingController.processAllRecentRevisionDumps();
-        //dumpProcessingController.processDump(dumpProcessingController.getMostRecentDump(DumpContentType.DAILY));
-        index.refreshReaders();
+        for (MwDumpFile dump : getNewDumpsToProcess(dumpProcessingController.getWmfDumpFileManager()).toArray(MwDumpFile[]::new)) {
+            dumpProcessingController.processDump(dump);
+            LAST_PROCESSED_DUMP_INFO.setDateStamp(dump.getDateStamp());
+            index.refreshReaders();
+        }
+        //TODO: test
 
         registerAtThreePM(() -> {
             try {
-                dumpProcessingController.processDump(dumpProcessingController.getMostRecentDump(DumpContentType.DAILY));
+                MwDumpFile dump = dumpProcessingController.getMostRecentDump(DumpContentType.DAILY);
+                dumpProcessingController.processDump(dump);
+                LAST_PROCESSED_DUMP_INFO.setDateStamp(dump.getDateStamp());
                 index.refreshReaders();
             } catch (IOException e) {
                 LOGGER.error(e.getMessage(), e);
             }
         });
+    }
+
+    private static Stream<MwDumpFile> getNewDumpsToProcess(WmfDumpFileManager dumpFileManager) {
+        return getPossibleDumpsToProcess(dumpFileManager).filter(dump ->
+                dump.getDateStamp().compareTo(LAST_PROCESSED_DUMP_INFO.getDateStamp()) > 0
+        );
+    }
+
+    private static Stream<MwDumpFile> getPossibleDumpsToProcess(WmfDumpFileManager dumpFileManager) {
+        MwDumpFile jsonDump = dumpFileManager.findMostRecentDump(DumpContentType.JSON);
+        return Stream.concat(
+                Stream.of(jsonDump),
+                dumpFileManager.findAllRelevantRevisionDumps(true).stream()
+                        .filter(dump -> Integer.parseInt(dump.getDateStamp()) > Integer.parseInt(jsonDump.getDateStamp())
+                        )).sorted(new MwDumpFile.DateComparator());
     }
 
     private static void registerAtThreePM(Runnable runnable) {
@@ -77,5 +102,30 @@ public class WikidataLuceneIndexFactory implements Factory<LuceneIndex> {
 
     @Override
     public void dispose(LuceneIndex luceneIndex) {
+    }
+
+    private static class LastProcessedDumpInfo {
+        private static final Path STORAGE_FILE = Paths.get(Configuration.getInstance().getLuceneDirectory(), "last-wd-dump-info");
+        private String dateStamp = "";
+
+        String getDateStamp() {
+            if (dateStamp.isEmpty()) {
+                try {
+                    dateStamp = Files.readAllLines(STORAGE_FILE).get(0);
+                } catch (IOException e) {
+                    //TODO: do something
+                }
+            }
+            return dateStamp;
+        }
+
+        void setDateStamp(String dateStamp) {
+            this.dateStamp = dateStamp;
+            try {
+                Files.write(STORAGE_FILE, Collections.singletonList(dateStamp));
+            } catch (IOException e) {
+                //TODO: do something?
+            }
+        }
     }
 }
