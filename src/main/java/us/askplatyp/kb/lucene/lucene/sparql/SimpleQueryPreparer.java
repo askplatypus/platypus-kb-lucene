dc6a334a7030215bf8a98e3948e13e605d942c36
Tpt <thomaspt@hotmail.fr>
2017-05-17 10:33:30 +0200

    Uses ErrorProne for static analysis

--- src/main/java/us/askplatyp/kb/lucene/lucene/sparql/SimpleQueryPreparer.java
+++ src/main/java/us/askplatyp/kb/lucene/lucene/sparql/SimpleQueryPreparer.java
@@ -31,7 +31,6 @@ import org.eclipse.rdf4j.query.algebra.evaluation.TripleSource;
 import org.eclipse.rdf4j.query.algebra.evaluation.federation.FederatedServiceResolverImpl;
 import org.eclipse.rdf4j.query.algebra.evaluation.impl.*;
 import org.eclipse.rdf4j.query.impl.EmptyBindingSet;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  * @author Thomas Pellissier Tanon
@@ -76,6 +75,6 @@ public class SimpleQueryPreparer extends AbstractQueryPreparer {
     protected void execute(
             UpdateExpr updateExpr, Dataset dataset, BindingSet bindings, boolean includeInferred, int maxExecutionTime
     ) throws UpdateExecutionException {
-        throw new NotImplementedException();
+        throw new UpdateExecutionException("This repository is read only");
     }
 }
